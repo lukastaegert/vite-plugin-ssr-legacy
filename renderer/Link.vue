ad24892ffa3a20390131ffdacac2c49d505a3a97
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2022-10-17 13:04:27 +0200

    Use explicit click handler

--- renderer/Link.vue
+++ renderer/Link.vue
@@ -1,8 +1,12 @@
 <template>
-  <a :class="{ active: pageContext.urlPathname === $attrs.href }">
+  <a
+    :class="{ active: pageContext.urlPathname === $attrs.href }"
+    @click="handleClick"
+  >
     <slot />
   </a>
 </template>
+
 <style scoped>
 a {
   padding: 3px 10px;
@@ -11,7 +15,19 @@ a.active {
   background-color: #eee;
 }
 </style>
+
 <script setup>
 import { usePageContext } from "./usePageContext";
+import { navigate } from "vite-plugin-ssr/client/router";
+import { useAttrs } from "vue";
+
 const pageContext = usePageContext();
+const attrs = useAttrs();
+
+// We are using an explicit click handler over automatic link detection to demonstrate the issue when importing "navigate"
+const handleClick = (event) => {
+  event.preventDefault();
+  event.stopPropagation();
+  navigate(attrs.href);
+};
 </script>
